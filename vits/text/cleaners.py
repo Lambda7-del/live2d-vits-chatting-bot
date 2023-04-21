82f383df1c48ff1f4591764712129331522ac1c0
Lambda7-del <85094717+Lambda7-del@users.noreply.github.com>
2023-04-21 13:49:41 +0800

    Update cleaners.py

--- vits/text/cleaners.py
+++ vits/text/cleaners.py
@@ -5,10 +5,6 @@ from .mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo,
 from .sanskrit import devanagari_to_ipa
 from .english import english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
 from .thai import num_to_thai, latin_to_thai
-from .shanghainese import shanghainese_to_ipa
-from .cantonese import cantonese_to_ipa
-from .ngu_dialect import ngu_dialect_to_ipa
-
 
 def japanese_cleaners(text):
     text = japanese_to_romaji_with_accent(text)
@@ -103,26 +99,3 @@ def thai_cleaners(text):
     text = latin_to_thai(text)
     return text
 
-
-def shanghainese_cleaners(text):
-    text = shanghainese_to_ipa(text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
-    return text
-
-
-def chinese_dialect_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_ipa2(x.group(1))+' ', text)
-    text = re.sub(r'\[JA\](.*?)\[JA\]',
-                  lambda x: japanese_to_ipa3(x.group(1)).replace('Q', 'ʔ')+' ', text)
-    text = re.sub(r'\[SH\](.*?)\[SH\]', lambda x: shanghainese_to_ipa(x.group(1)).replace('1', '˥˧').replace('5',
-                  '˧˧˦').replace('6', '˩˩˧').replace('7', '˥').replace('8', '˩˨').replace('ᴀ', 'ɐ').replace('ᴇ', 'e')+' ', text)
-    text = re.sub(r'\[GD\](.*?)\[GD\]',
-                  lambda x: cantonese_to_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[EN\](.*?)\[EN\]',
-                  lambda x: english_to_lazy_ipa2(x.group(1))+' ', text)
-    text = re.sub(r'\[([A-Z]{2})\](.*?)\[\1\]', lambda x: ngu_dialect_to_ipa(x.group(2), x.group(
-        1)).replace('ʣ', 'dz').replace('ʥ', 'dʑ').replace('ʦ', 'ts').replace('ʨ', 'tɕ')+' ', text)
-    text = re.sub(r'\s+$', '', text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
-    return text
